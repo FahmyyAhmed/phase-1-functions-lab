f6c8246fd9fa3946b8c4cf5f95f4a1e808b8911c
Fahmy Ahmed <fahmy.ahmed@student.moringaschool.com>
2025-03-18 16:32:05 +0300

    done with functions

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+
+let distance;
+
+function distanceFromHqInBlocks(pickup) {
+  const block = 42;
+  distance = Math.abs(block - pickup);
+  return distance;
+}
+
+
+function distanceFromHqInFeet(pickup) {
+  return distanceFromHqInBlocks(pickup) * 264;
+}
+
+
+function distanceTravelledInFeet(start, finish) {
+  return Math.abs(start - finish) * 264;
+}
+
+
+function calculatesFarePrice(start, finish) {
+  if (distanceTravelledInFeet(start, finish) <= 400) {
+    return 0;
+  } else if (distanceTravelledInFeet(start, finish) >= 400) {
+    if (distanceTravelledInFeet(start, finish) <= 2000) {
+      return (Math.abs(start - finish) * 264 - 400) * 0.02;
+    } else if (distanceTravelledInFeet(start, finish) < 2500) {
+      return 25;
+    } else if (distanceTravelledInFeet(start, finish) >= 2500) {
+      return "cannot travel that far";
+    }
+  }
+}
